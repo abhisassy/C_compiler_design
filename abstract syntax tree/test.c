53528e24818d2fd405f3eaed6bfb536f20d49b7c
abhisassy <abbhi.goku@gmail.com>
2019-03-28 02:19:33 +0530

    abs started

---
+++ abstract syntax tree/test.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+void fun(int x, float cc){
+	int c=1+1;
+}
+int main(){
+	char c=97;
+	float i=1;
+	i=2;
+	i=3;
+	i=4;
+	{i=5.5; float x = 12.1e-1;}
+	//float x;
+	//x=1+2*2-1;
+	int x;
+	printf("");
+	while(x>1){
+		int y;
+		break;
+		int z;
+		continue;
+	}
+
+	//printf("%d",x);
+	/*int i;
+	while(1>10){
+		int a=10;
+		int b=12;
+		{
+			int x=0;
+			int i=9;
+		}
+		{
+				int s=7;
+		}
+
+
+	}*/
+	return 0;
+}
